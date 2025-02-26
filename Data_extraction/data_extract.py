bbe14acb3938c9aff3f0ae98f7c747c98038328f
Anupama-nambiar <annambiar@ucsd.edu>
2025-02-26 15:59:05 -0800

    Ordinal_encoding_for_frequency

--- Data_extraction/data_extract.py
+++ Data_extraction/data_extract.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+
 def data_extracting(x):
         '''
         takes a file-name and creates new file with required data 
@@ -10,12 +11,25 @@ def data_extracting(x):
 
         #reading the required csv file for extraction of data
         data=pd.read_csv(x)
-        print(data)
+       
 
-        #created df with selected columns
-        select= data[['Heating Energy Source',  "Vehicle Type", "Transport",'Frequency of Traveling by Air', 'CarbonEmission']]
+        
+        #    has less brute-force but it is not in any particular order
+        # value_s= data['Frequency of Traveling by Air'].unique().tolist()
+
+        dict_of_encoding={'never':0, 'rarely': 1,'frequently': 2,'very frequently': 3}
+        #                           or
+        # dict_of_encoding = {key: value_s.index(key) for key in value_s}
+        
+        
+        #ordinal encoding for 'Frequency of Traveling by Air' column -------if we want rarely to be 0 and so on 
+        data['Frequency of Traveling by Air'].replace(dict_of_encoding, inplace=True) # ordinally encoding the column
+
+
 
         #saves data extracted in a csv file named extracted_data
+        #created df with selected columns
+        select= data[['Heating Energy Source',  "Vehicle Type", "Transport",'Frequency of Traveling by Air', 'Vehicle Monthly Distance Km' ,'CarbonEmission',]]
         file_path = os.path.join(cwd, "Data_extraction/extracted_data.csv")
         select.to_csv(file_path, index=False)
 
