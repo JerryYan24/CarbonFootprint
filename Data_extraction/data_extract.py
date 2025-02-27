705114f59a441aa78ef7dfbc84c6d6e0fc52307c
Anupama-nambiar <annambiar@ucsd.edu>
2025-02-26 16:34:18 -0800

    edited_encoding_code

--- Data_extraction/data_extract.py
+++ Data_extraction/data_extract.py
@@ -8,28 +8,36 @@ def data_extracting(x):
         :input: x : filename=str
         '''
         cwd = os.getcwd()
-
-        #reading the required csv file for extraction of data
-        data=pd.read_csv(x)
+        columns_to_select=['Heating Energy Source',  "Vehicle Type", "Transport",'Frequency of Traveling by Air', 'Vehicle Monthly Distance Km' ,'CarbonEmission']
+       
+        
+        data=pd.read_csv(x) #reading the required csv file for extraction of data
        
 
-        
-        #    has less brute-force but it is not in any particular order
-        # value_s= data['Frequency of Traveling by Air'].unique().tolist()
-
-        dict_of_encoding={'never':0, 'rarely': 1,'frequently': 2,'very frequently': 3}
-        #                           or
-        # dict_of_encoding = {key: value_s.index(key) for key in value_s}
-        
-        
-        #ordinal encoding for 'Frequency of Traveling by Air' column -------if we want rarely to be 0 and so on 
+        # ENCODING FREQUENCY COLUMN
+        # value_Frequency_of_Traveling_by_Air= data['Frequency of Traveling by Air'].unique().tolist()  
+       # dict_of_encoding = {key: value_s.index(key) for key in value_s} #this has less brute-force but it is not in any particular order
+        dict_of_encoding={'never':0, 'rarely': 1,'frequently': 2,'very frequently': 3} #brute force but in order
         data['Frequency of Traveling by Air'].replace(dict_of_encoding, inplace=True) # ordinally encoding the column
+        
 
 
 
-        #saves data extracted in a csv file named extracted_data
+        # ENCODING TRANSPORT COLUMN
+        value_transport= data['Transport'].unique().tolist() #values in column
+        columns_to_select+=value_transport #adding column names to selection list
+        for i in value_transport:
+                data[i] = data['Transport'].apply(lambda x: 1 if x == i else 0) #hot-encoding
+            
+        columns_to_select.remove('Transport') #removing transport column name from the required selectionn list
+
+
+
+
+
+        # SAVING DATA
         #created df with selected columns
-        select= data[['Heating Energy Source',  "Vehicle Type", "Transport",'Frequency of Traveling by Air', 'Vehicle Monthly Distance Km' ,'CarbonEmission',]]
+        select= data[columns_to_select]
         file_path = os.path.join(cwd, "Data_extraction/extracted_data.csv")
         select.to_csv(file_path, index=False)
 
